88657c74385ff7948f4d6512f97389c4581a9237
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-01-22 18:54:12 +0700

    test

--- src/config.c
+++ src/config.c
@@ -1,6 +1,6 @@
 #ifndef _CONFIG_
 #define _CONFIG_
 
-#define _CONFIG_TCP_SPACE_BETWEEN_TIME_CLEANUP 4 //  in sec
-#define _CONFIG_TCP_MAX_CONN 100
+#define _CONFIG_TCP_SPACE_BETWEEN_TIME_CLEANUP 5 //  in sec
+#define _CONFIG_TCP_MAX_CONN 10
 #endif
