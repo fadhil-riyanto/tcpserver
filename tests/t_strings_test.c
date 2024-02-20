963e322076f1922e36160b20d204ac3904fe5c46
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-02-20 21:03:31 +0700

    fix bug, add return code

--- tests/t_strings_test.c
+++ tests/t_strings_test.c
@@ -6,8 +6,10 @@
 
 /*
  * ret
+* 0 eq literating
  * 1 eq last
- * 0 eq literating
+ * 2 not found
+ * 3 index overflow
  */
 int split(char separator, char *txt, int size, int return_index_of, char **dest)
 {
@@ -40,14 +42,18 @@ int split(char separator, char *txt, int size, int return_index_of, char **dest)
             foundindex = foundindex + 1;
 
         } else if (txt[i+ 1] == '\0') {
-            for(int c = (index - (counter)); c < (index); c++) {
-                chartemp[a] = txt[c + split_loop];
+            for(int c = (index - (counter)); c < (index + 1); c++) {
+                chartemp[a] = txt[c + split_loop ];
                 a = a + 1;
             }
             chartemp[a] = '\0'; 
             if (foundindex == return_index_of) {
                 printf("indexof %d: \"%s\"\n", return_index_of, chartemp);
-                return 0;
+                return 1;
+            }
+            if (return_index_of > foundindex)
+            {
+                return 3;
             }
             foundindex = foundindex + 1;
             
@@ -56,16 +62,19 @@ int split(char separator, char *txt, int size, int return_index_of, char **dest)
             index = index + 1;
         }
     }
+    return 2;
 }
+
 int main()
 {
-    char *txt = "abbbbbc:sssssssss:weeeeeeeeeeew:eeeeee:rrrrrrrrrrrr:wqqqqqqqqqqqqqqqqqqqqqqqq we2";
+    char *txt = "";
     char *dest;
+    int ret;
 
     // for(int i = 0; i < 4; i++)
     // {
-        split(':', txt, strlen(txt), 5, &dest);
+    ret = split(':', txt, strlen(txt), 2, &dest);
     // }
 
-    // printf("\" %s\"\n", dest);
+    printf("\" %d\"\n", ret);
 }
